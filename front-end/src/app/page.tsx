0449c65c9e66aaaf24201203a96b7ea20f2f67eb
Тэнгис Батболд <24LP8615@G401-CC0096.local>
2024-11-15 09:13:13 +0800

    Tengis-Food-Delivery

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -7,29 +7,31 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import AdbIcon from '@mui/icons-material/Adb';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import Stack from '@mui/material/Stack';
 
 export default function Home() {
-
-
-const pages = ['Нүүр', 'ХООЛНЫ ЦЭС', 'ХҮРГЭЛТИЙН БҮС'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+  const pages = ['Нүүр', 'ХООЛНЫ ЦЭС', 'ХҮРГЭЛТИЙН БҮС'];
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
+  
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
+  
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
+  
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+  
   const fetchData = async () => {
     try {
       const response = await fetch("http://localhost:8000 ");
@@ -39,65 +41,49 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
       console.log(error);
     }
   };
+  
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
-      <Container maxWidth="xl" sx={{ color: 'inherit',}}>
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'black',
-              textDecoration: 'none',
-            }}
+    <Container maxWidth="xl" sx={{ color: 'inherit' }}>
+      <Toolbar disableGutters>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="#app-bar-with-responsive-menu"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'black',
+            textDecoration: 'none',
+          }}
+        >
+          FOODDELIVERY
+        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' }, ml: 10, gap: 5,  }}
           >
-            FOODDELIVERY
-          </Typography>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'black', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-          <Stack>
-           <IconButton color="primary" aria-label="add to shopping cart">
-              <AddShoppingCartIcon />
-           </IconButton>
-          </Stack>
-        </Toolbar>
-      </Container>
-  );}
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{ my: 5, color: 'black', display: 'block' }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+        <Stack>
+          <IconButton color="black" aria-label="add to shopping cart" sx={{ ml: -10 }}>
+            <AddShoppingCartIcon />
+          </IconButton>
+        </Stack>
+      </Toolbar>
+    </Container>
+  );
+}
