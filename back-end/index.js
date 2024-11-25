44bece8d5e46d7836387d293f015e716427c22d8
Тэнгис Батболд <24LP8615@Users-iMac-2.local>
2024-11-25 13:08:42 +0800

    Tengis-Food-Delivery

--- back-end/index.js
+++ back-end/index.js
@@ -1,67 +1,16 @@
 import express from "express";
 import cors from "cors";
-import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv";
-import connectDB from "./ConnectDB.js";
-import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
+import userRouter from "./router/userRouter.js";
 
-dotenv.config();
-
+mongoose.connect(
+  "mongodb+srv://tengisbatbold11:Tengis0808@tengis-food.psict.mongodb.net/"
+);
 const server = express();
-const PORT = 8000;
+const PORT = 4000;
 
 server.use(cors());
-server.post("/image-upload", async (request, response) => {
-  try {
-    cloudinary.config({
-      cloud_name: "dwm2mie3c",
-      api_key: "257275265174362",
-      api_secret: "V_k2DSUgZStwgIunis7GTWNraJA",
-    });
-
-    const uploadResult = await cloudinary.uploader.upload("./asset/budaa.png");
-    response.json(uploadResult);
-  } catch (error) {
-    console.log("cloudinary error", error);
-  }
-});
-
-server.get("/", async (req, response) => {
-  try {
-    const db = await connectDB();
-    const collection = db.collection("users");
-    const results = await collection.findOne({ name: "Ned Stark" });
-    response.json({
-      success: true,
-      data: results,
-    });
-  } catch (error) {
-    console.error("Error in GET /:", error);
-    response.status(500).json({
-      success: false,
-      message: "Internal Server Error",
-    });
-  }
-});
-
-server.post("/image-upload", async (request, response) => {
-  try {
-    cloudinary.config({
-      cloud_name: "dwm2mie3c",
-      api_key: "257275265174362",
-      api_secret: "V_k2DSUgZStwgIunis7GTWNraJA",
-    });
-
-    const uploadResult = await cloudinary.uploader.upload("./asset/budaa.png");
-    response.json(uploadResult);
-  } catch (error) {
-    console.log("Cloudinary error", error);
-    response.status(500).json({
-      success: false,
-      message: "Error uploading image",
-    });
-  }
-});
+server.use("/api", userRouter);
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
