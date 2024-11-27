82a4c60d69a6eda8aff844916a593313d052ac78
Тэнгис Батболд <24LP8615@Users-iMac-2.local>
2024-11-27 11:07:40 +0800

    Tengis-Food-Delivery

---
+++ back-end/orderControl.js
@@ -0,0 +1,25 @@
+import { request, response } from "express";
+import { Order } from "./model/order";
+
+const createOrder = async (request, response) => {
+  const result = await Order.create({
+    orderNumber: 1,
+    totalPrice: "215000",
+    customer: "6743f553f7907131039502db",
+  });
+
+  response.json({
+    succes: true,
+    data: result,
+  });
+};
+
+const getAllOrders = async (request, response) => {
+  const result = await Order.find().populate("customer");
+
+  response.json({
+    succes: true,
+    data: result,
+  });
+};
+export { createOrder, getAllOrders };
