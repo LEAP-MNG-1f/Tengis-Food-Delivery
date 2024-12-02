024f9b3aa345cc6af88c79eea9573c0d4511bcd3
Тэнгис Батболд <24LP8615@Users-iMac-2.local>
2024-12-02 09:12:03 +0800

    Tengis-Food-Delivery

--- front-end/src/app/components/menu/soup.tsx
+++ front-end/src/app/components/menu/soup.tsx
@@ -109,22 +109,6 @@ export default function Soup() {
                             >
                               Сагслах
                             </Button>
-                            {/* <div>
-                              {(["right"] as const).map((anchor) => (
-                                <React.Fragment key={anchor}>
-                                  <Button onClick={toggleDrawer(anchor, true)}>
-                                    {anchor}
-                                  </Button>
-                                  <Drawer
-                                    anchor={anchor}
-                                    open={state[anchor]}
-                                    onClose={toggleDrawer(anchor, false)}
-                                  >
-                                    {list(anchor)}
-                                  </Drawer>
-                                </React.Fragment>
-                              ))}
-                            </div> */}
                           </div>
                         </div>
                       </div>
